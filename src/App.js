2918a92f9c1c6acb8387b26a77ad6c295469c026
phongnv-vn <78303085+phongnv-vn@users.noreply.github.com>
2023-01-31 14:17:10 +0700

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import "./style.css";
+import { Typography, Divider } from 'antd';
+import TodoList from './components/TodoList';
+import Filters from './components/Filters';
 
-export default function App() {
+const { Title } = Typography;
+
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div
+      style={{
+        width: 500,
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        padding: 20,
+        boxShadow: '0 0 10px 4px #bfbfbf',
+        borderRadius: 5,
+        height: '90vh',
+      }}
+    >
+      <Title style={{ textAlign: 'center' }}>TODO APP with REDUX</Title>
+      <Filters />
+      <Divider />
+      <TodoList />
     </div>
   );
 }
+
+export default App;
